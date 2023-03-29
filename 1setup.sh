23f28b08f89bac38fb3a5672e1f721d456bcab7f
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2023-03-29 15:44:23 -0400

    Update 1setup.sh

--- 1setup.sh
+++ 1setup.sh
@@ -8,29 +8,7 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # user name, password, etc.
 
 
-    pacman-key --init
-    pacman-key --populate archlinux
-    pacman -Sy archlinux-keyring --needed --noconfirm
-    sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
     
-    #Might have to resort to using this if I can't figure out chaotic-aur signing consistently
-    #sed -i 's/^SigLevel    = Required DatabaseOptional/SigLevel    = Never/' /etc/pacman.conf
-
-    pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-    pacman-key --lsign-key FBA220DFC880C036
-    pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
-    cat $SCRIPT_DIR/mirror.txt >> /etc/pacman.conf
-    pacman -Sy  chaotic-keyring --needed --noconfirm
-    pacman -S --noconfirm --needed btrfs-progs gptfdisk reflector rsync glibc
-    timedatectl set-ntp true
-    echo -ne "
--------------------------------------------------------------------------
-                    Updating Mirrorlist
--------------------------------------------------------------------------
-"
-    reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
-
-    clear
 
 # set up a config file
 CONFIG_FILE=$SCRIPT_DIR/config.sh
@@ -38,55 +16,6 @@ source $SCRIPT_DIR/config.sh
 
 
 
-timezone () {
-# Added this from arch wiki https://wiki.archlinux.org/title/System_time
-time_zone="$(curl --fail https://ipapi.co/timezone)"
-echo -ne "
-System detected your timezone to be '$time_zone' \n"
-echo -ne "Is this correct?
-"
-options=("Yes" "No")
-select_option $? 1 "${options[@]}"
-
-case ${options[$?]} in
-    y|Y|yes|Yes|YES)
-    echo "${time_zone} set as timezone"
-    set_option TIMEZONE $time_zone;;
-    n|N|no|NO|No)
-    echo "Please enter your desired timezone e.g. Europe/London :"
-    read new_timezone
-    echo "${new_timezone} set as timezone"
-    set_option TIMEZONE $new_timezone;;
-    *) echo "Wrong option. Try again";timezone;;
-esac
-}
-
-localeselect () {
-echo -ne "
-Please select your locale from this list"
-# These are default key maps as presented in official arch repo archinstall
-options=(en_CA.UTF-8 en_HK.UTF-8 en_US.UTF-8 fr_CA.UTF-8 fr_FR.UTF-8 zh_CN.UTF-8 zh_TW.UTF-8 hu.UTF-8 it_IT.UTF-8 ja_JP.UTF-8 ru_RU.UTF-8 es_ES.UTF-8 de_DE.UTF-8 ar_SA.UTF-8 af_ZA.UTF-8)
-
-select_option $? 4 "${options[@]}"
-langlocale=${options[$?]}
-
-echo -ne "Your locale: ${langlocale} \n"
-echo -ne "Is this correct?
-"
-options=("Yes" "No")
-select_option $? 1 "${options[@]}"
-
-case ${options[$?]} in
-    y|Y|yes|Yes|YES)
-    set_option LANGLOCAL $langlocale;;
-    n|N|no|NO|No)
-    clear
-    echo "Please choose again"
-    localeselect;;
-    *) echo "Wrong option. Try again";localeselect;;
-esac
-
-}
 
 
 keymap () {
@@ -116,192 +45,6 @@ esac
 
 }
 
-loginshell () {
-echo -ne "
-Please select a shell from this list for using with your user (root will still use bash by default)"
-
-options=(bash fish zsh)
-
-select_option $? 4 "${options[@]}"
-shellchoice=${options[$?]}
-
-echo -ne "Your shell : ${shellchoice} \n"
-echo -ne "Is this correct?
-"
-options=("Yes" "No")
-select_option $? 1 "${options[@]}"
-
-case ${options[$?]} in
-    y|Y|yes|Yes|YES)
-set_option SHELLCHOICE $shellchoice;;
-    n|N|no|NO|No)
-    clear
-    echo "Please choose again"
-    loginshell;;
-    *) echo "Wrong option. Try again";loginshell;;
-esac
-
-}
-
-
-desktopenv () {
-echo -ne "
-Please select an Environement from this list"
-options=(kaidaplasma fullplasma minimalplasma gnome fullgnome xfce fullxfce fullMATE MATE cinnamon fulldeepin deepin lxqt i3gaps xmonad openbox bspwm none)
-
-select_option $? 4 "${options[@]}"
-dechoice=${options[$?]}
-
-echo -ne "Your Environement : ${dechoice} \n"
-echo -ne "Is this correct?
-"
-options=("Yes" "No")
-select_option $? 1 "${options[@]}"
-
-case ${options[$?]} in
-    y|Y|yes|Yes|YES)
-set_option DECHOICE $dechoice;;
-    n|N|no|NO|No)
-    clear
-    echo "Please choose again"
-    desktopenv;;
-    *) echo "Wrong option. Try again";desktopenv;;
-esac
-
-
-}
-
-kernelselect () {
-echo -ne "
-Please select a kernel from this list"
-options=(linux linux-zen linux-hardened linux-lts)
-
-select_option $? 4 "${options[@]}"
-kernelchoice=${options[$?]}
-
-echo -ne "Your kernel : ${kernelchoice} \n"
-echo -ne "Is this correct?
-"
-options=("Yes" "No")
-select_option $? 1 "${options[@]}"
-
-case ${options[$?]} in
-    y|Y|yes|Yes|YES)
-set_option KERNELCHOICE $kernelchoice;;
-    n|N|no|NO|No)
-    clear
-    echo "Please choose again"
-    kernelselect;;
-    *) echo "Wrong option. Try again";kernelselect;;
-esac
-
-}
-
-lib32repo () {
-echo -ne "
-Do you want the Multilib repo ?"
-options=(no yes)
-
-select_option $? 4 "${options[@]}"
-libchoice=${options[$?]}
-
-echo -ne "Your choice : ${libchoice} \n"
-echo -ne "Is this correct?
-"
-options=("Yes" "No")
-select_option $? 1 "${options[@]}"
-
-case ${options[$?]} in
-    y|Y|yes|Yes|YES)
-set_option LIBCHOICE $libchoice;;
-    n|N|no|NO|No)
-    clear
-    echo "Please choose again"
-    lib32repo;;
-    *) echo "Wrong option. Try again";blackarch;;
-esac
-
-}
-
-AurHelper () {
-echo -ne "
-Please select an aur helper from this list"
-options=(none yay paru octopi-paru octopi-yay)
-
-select_option $? 4 "${options[@]}"
-aurchoice=${options[$?]}
-
-echo -ne "Your choice : ${aurchoice} \n"
-echo -ne "Is this correct?
-"
-options=("Yes" "No")
-select_option $? 1 "${options[@]}"
-
-case ${options[$?]} in
-    y|Y|yes|Yes|YES)
-set_option AURCHOICE $aurchoice;;
-    n|N|no|NO|No)
-    clear
-    echo "Please choose again"
-    AurHelper;;
-    *) echo "Wrong option. Try again";AurHelper;;
-esac
-
-}
-
-
-
-chaorepo () {
-echo -ne "
-Do you want the Chaotic-Aur repo ?"
-options=(no yes)
-
-select_option $? 4 "${options[@]}"
-chaochoice=${options[$?]}
-
-echo -ne "Your choice : ${chaochoice} \n"
-echo -ne "Is this correct?
-"
-options=("Yes" "No")
-select_option $? 1 "${options[@]}"
-
-case ${options[$?]} in
-    y|Y|yes|Yes|YES)
-    set_option CHAOCHOICE $chaochoice;;
-    n|N|no|NO|No)
-    clear
-    echo "Please choose again"
-    chaorepo;;
-    *) echo "Wrong option. Try again";blackarch;;
-esac
-
-}
-
-blackarch () {
-echo -ne "
-Do you want the BlackArch repo ?"
-options=(no yes)
-
-select_option $? 4 "${options[@]}"
-blackchoice=${options[$?]}
-
-echo -ne "Your choice : ${blackchoice} \n"
-echo -ne "Is this correct?
-"
-options=("Yes" "No")
-select_option $? 1 "${options[@]}"
-
-case ${options[$?]} in
-    y|Y|yes|Yes|YES)
-set_option BLACKCHOICE $blackchoice;;
-    n|N|no|NO|No)
-    clear
-    echo "Please choose again"
-    blackarch;;
-    *) echo "Wrong option. Try again";blackarch;;
-esac
-
-}
 
 userinfo () {
 read -p "Please enter your username: " username
@@ -517,12 +260,6 @@ homepartition () {
     myhostname
     clear
     logo
-    timezone
-    clear
-    logo
-    localeselect
-    clear
-    logo
     lsblk
     read -p "Please enter your EFI partition (EX: /dev/sda1): " partition2
     set_option EFIPART $partition2
@@ -547,24 +284,3 @@ homepartition () {
     mkfs.btrfs -L ROOT -m single -f $partition3
     uuid3=$(blkid -o value -s UUID $partition3)
     set_option ROOTUUID $uuid3
-    clear
-    logo
-    loginshell
-    clear
-    logo
-    desktopenv
-    clear
-    logo
-    kernelselect
-    clear
-    logo
-    lib32repo
-    clear
-    logo
-    AurHelper
-    clear
-    logo
-    chaorepo
-    clear
-    logo
-    blackarch
