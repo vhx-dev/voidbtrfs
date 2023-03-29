8298ffa24c4dacce98f2ae8a2dcf87afa35213c0
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2023-03-29 16:27:03 -0400

    Update 5final.sh

--- 5final.sh
+++ 5final.sh
@@ -10,52 +10,10 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 echo "$NAME_OF_MACHINE" > /etc/hostname
-ln -s /usr/share/zoneinfo/$TIMEZONE /etc/localtime
-hwclock --systohc 
-# Set keymaps
 echo KEYMAP=$KEYMAP > /etc/vconsole.conf
 loadkeys $KEYMAP
 echo "LANG=${LANGLOCAL}" > /etc/locale.conf
 
-echo -ne "
-
--------------------------------------------------------------------------
-        Updating full system and Setting up octopi and Aur Helper
--------------------------------------------------------------------------
-"
-
-    if [ "$CHAOCHOICE" = "yes" ]; then
-
-#adding chaotic-Aur and Black Arch Repo
-pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key FBA220DFC880C036
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
-cat /root/archscript/mirror.txt >> /etc/pacman.conf
-pacman -Sy --noconfirm
-fi
-
-    if [ "$BLACKCHOICE" = "yes" ]; then
-
-    curl -O https://blackarch.org/strap.sh
-    chmod +x strap.sh
-    bash strap.sh
-    rm /strap.sh
-    pacman -Syyu --noconfirm
-    fi
-
-
-if [ "$AURCHOICE" = "yay" ]; then
-      pacman -S yay --noconfirm
-      
-      elif [ "$AURCHOICE" = "paru" ]; then
-      pacman -S paru --noconfirm
-      
-      elif [ "$AURCHOICE" = "octopi-paru" ]; then
-      pacman -S paru octopi --noconfirm
-      
-      elif [ "$AURCHOICE" = "octopi-yay" ]; then
-      pacman -S yay octopi --noconfirm
-fi
 
 
 
@@ -71,8 +29,8 @@ sed -i 's|TIMELINE_LIMIT_YEARLY="10"|TIMELINE_LIMIT_YEARLY="0"|' /etc/snapper/co
 
 
 #activating the auto-cleanup
-SCRUB=$(systemd-escape --template btrfs-scrub@.timer --path /dev/disk/by-uuid/${ROOTUUID})
-systemctl enable ${SCRUB}
-systemctl enable snapper-timeline.timer
-systemctl enable snapper-cleanup.timer
+#SCRUB=$(systemd-escape --template btrfs-scrub@.timer --path /dev/disk/by-uuid/${ROOTUUID})
+#systemctl enable ${SCRUB}
+#systemctl enable snapper-timeline.timer
+#systemctl enable snapper-cleanup.timer
 
