50f3dcfc20ec620121942b9af0f924c03d8d5aa8
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2023-03-29 15:53:34 -0400

    Update 3strap.sh

--- 3strap.sh
+++ 3strap.sh
@@ -17,8 +17,10 @@ echo -ne "
 "
 
 
+mkdir -p /mnt/var/db/xbps/keys
+cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
 
-
+XBPS_ARCH=x86_64 xbps-install -S -r /mnt -R "https://repo-default.voidlinux.org/current" base-system
 
 
 clear
