ab398250ff66999bc4f9065732b02704d7f93278
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2023-03-30 00:06:52 -0400

    Update 5final.sh

--- 5final.sh
+++ 5final.sh
@@ -14,8 +14,9 @@ echo KEYMAP=$KEYMAP > /etc/vconsole.conf
 loadkeys $KEYMAP
 echo "LANG=${LANGLOCAL}" > /etc/locale.conf
 
-
-
+echo -e "hostonly=yes\nhostonly_cmdline=yes" >> /etc/dracut.conf.d/00-hostonly.conf
+echo "add_dracutmodules+=\" btrfs resume \"" >> /etc/dracut.conf.d/20-addmodules.conf
+echo "tmpdir=/tmp" >> /etc/dracut.conf.d/30-tmpfs.conf
 
 #Changing The timeline auto-snap
 sed -i 's|QGROUP=""|QGROUP="1/0"|' /etc/snapper/configs/root
