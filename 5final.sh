c6516522a044140aacb6199332c5c81fc37d58e7
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2023-03-29 23:38:21 -0400

    Update 5final.sh

--- 5final.sh
+++ 5final.sh
@@ -21,16 +21,9 @@ echo "LANG=${LANGLOCAL}" > /etc/locale.conf
 sed -i 's|QGROUP=""|QGROUP="1/0"|' /etc/snapper/configs/root
 sed -i 's|NUMBER_LIMIT="50"|NUMBER_LIMIT="5-15"|' /etc/snapper/configs/root
 sed -i 's|NUMBER_LIMIT_IMPORTANT="50"|NUMBER_LIMIT_IMPORTANT="5-10"|' /etc/snapper/configs/root
-sed -i 's|TIMELINE_LIMIT_HOURLY="10"|TIMELINE_LIMIT_HOURLY="2"|' /etc/snapper/configs/root
-sed -i 's|TIMELINE_LIMIT_DAILY="10"|TIMELINE_LIMIT_DAILY="2"|' /etc/snapper/configs/root
-sed -i 's|TIMELINE_LIMIT_WEEKLY="0"|TIMELINE_LIMIT_WEEKLY="2"|' /etc/snapper/configs/root
+sed -i 's|TIMELINE_LIMIT_HOURLY="10"|TIMELINE_LIMIT_HOURLY="0"|' /etc/snapper/configs/root
+sed -i 's|TIMELINE_LIMIT_DAILY="10"|TIMELINE_LIMIT_DAILY="1"|' /etc/snapper/configs/root
+sed -i 's|TIMELINE_LIMIT_WEEKLY="0"|TIMELINE_LIMIT_WEEKLY="1"|' /etc/snapper/configs/root
 sed -i 's|TIMELINE_LIMIT_MONTHLY="10"|TIMELINE_LIMIT_MONTHLY="0"|' /etc/snapper/configs/root
 sed -i 's|TIMELINE_LIMIT_YEARLY="10"|TIMELINE_LIMIT_YEARLY="0"|' /etc/snapper/configs/root
-
-
-#activating the auto-cleanup
-#SCRUB=$(systemd-escape --template btrfs-scrub@.timer --path /dev/disk/by-uuid/${ROOTUUID})
-#systemctl enable ${SCRUB}
-#systemctl enable snapper-timeline.timer
-#systemctl enable snapper-cleanup.timer
 
